610eeda0cc36629a974617810cbbe98f3b0c73ac
lobart <lobachav@gmail.com>
2021-11-22 16:56:47 +0300

    add init sh

--- SmartCityAdmin/settings.py
+++ SmartCityAdmin/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', 5432),
         'OPTIONS': {
            'options': '-c search_path=public,content'
---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DB_NAME=smc
+export DB_USER=smc
+export DB_PASSWORD=secret
+export DB_HOST=10.20.0.244

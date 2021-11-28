b8e7c363c9d57ccd8c2ec94d3b702744cde66542
Lobach <artem.lobach@bg-optics.ru>
2021-11-28 23:32:08 +0300

    add logger to faces models

--- src/smartcity/SmartCityAdmin/settings.py
+++ src/smartcity/SmartCityAdmin/settings.py
@@ -94,8 +94,34 @@ DATABASES = {
 }
 
 LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'filters': [],
+        }
+    },
     'loggers': {
-        'faces.models': {
+        'faces': {
             'level': 'DEBUG',
             'handlers': ['console'],
         },

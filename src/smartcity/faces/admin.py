34a28beba7ac7f6f1918eda46bfe741a78d58873
Lobach <artem.lobach@bg-optics.ru>
2021-11-28 10:47:14 +0300

    add image load

--- src/smartcity/faces/admin.py
+++ src/smartcity/faces/admin.py
@@ -5,6 +5,5 @@ from django.forms import ModelForm
 
 @admin.register(Profiles)
 class ProfilesAdmin(admin.ModelAdmin, ModelForm):
-    list_display = ("employee_id", "full_name", "drawing_data")
     list_filter = ("drawing_data")
     fields = ("employee_id", "full_name", "drawing_data")

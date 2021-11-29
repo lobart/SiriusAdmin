3572601895088bfa33290068ee679d7c7a951703
Lobach <artem.lobach@bg-optics.ru>
2021-11-29 05:18:57 +0300

    add image load

--- src/smartcity/SmartCityAdmin/urls.py
+++ src/smartcity/SmartCityAdmin/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from src.smartcity.faces.views import PersonDetailView
+from faces.views import PersonDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('<slug:slug>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('<slug:slug>/', PersonDetailView.as_view(), name='article-detail'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

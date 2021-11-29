bbee4560c86c617c48a8138ef0bf82eb1023778d
Lobach <artem.lobach@bg-optics.ru>
2021-11-29 05:27:02 +0300

    add image load

--- src/smartcity/faces/admin.py
+++ src/smartcity/faces/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from .models import Profiles
+import io
+from django.core.files.images import ImageFile
 
+def create_image_message(image_bytes, name):
+    image = ImageFile(io.BytesIO(image_bytes), name=name)
+    new_message = Profiles.objects.create(image=image)
+    return new_message
 
 @admin.register(Profiles)
 class ProfilesAdmin(admin.ModelAdmin):
     list_display = ("employee_id", "full_name","drawing_data")
-    fields = ("employee_id", "full_name")
+    fields = ("employee_id", "full_name", 'image')
+    readonly_fields = ['image']
+
+    def render_change_form(self, request, context, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['image'] = create_image_message(context['drawing_data'], context['employee_id'] + '.jpg')
+        return super(ProfilesAdmin, self).render_change_form(request, context, args, kwargs)
 
 
 

24b295026e49e933e057644fab12c90789f9d38a
Lobach <artem.lobach@bg-optics.ru>
2021-11-29 04:28:19 +0300

    add image load

--- src/smartcity/faces/models.py
+++ src/smartcity/faces/models.py
@@ -29,6 +29,8 @@ class Profiles(models.Model):
         name = '{id}'.format(id=self.employee_id) + '.jpg'
         image = models.FileField(io.BytesIO(bytes(self.drawing_data)), name=name,
                                       upload_to=str(settings.MEDIA_ROOT), null=True)
+        storage = image.storage
+        data = image.description
         if image:
             logger.info(image)
             return "Image url is %s" % image.url

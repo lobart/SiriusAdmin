bf130cf2e4e2a8c01bc7ef01b0b88f9c852f346f
Lobach <artem.lobach@bg-optics.ru>
2021-11-29 03:53:25 +0300

    add image load

--- src/smartcity/faces/models.py
+++ src/smartcity/faces/models.py
@@ -35,7 +35,8 @@ class Profiles(models.Model):
         image = models.FileField(io.BytesIO(bytes(self.drawing_data)), name=name,
                                       upload_to=str(settings.MEDIA_ROOT), null=True)
         html = '<img src="{img}">'
-        logger.info("Image url is %s"%image.url)
+        logger.info("Image url is %s" % image.url)
+        logger.error("Image url is %s" % image.url)
         if image:
             return format_html(html, img=image.url)
         return format_html('<strong>There is no image for this entry.<strong>')

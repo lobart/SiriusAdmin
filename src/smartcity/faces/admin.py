9b035401b7d0866c2eff7b26c7670865dd50e3ea
Lobach <artem.lobach@bg-optics.ru>
2021-11-29 04:15:44 +0300

    add image load

--- src/smartcity/faces/admin.py
+++ src/smartcity/faces/admin.py
@@ -5,8 +5,7 @@ from .models import Profiles
 @admin.register(Profiles)
 class ProfilesAdmin(admin.ModelAdmin):
     list_display = ("employee_id", "full_name","drawing_data")
-    fields = ("employee_id", "full_name", 'display_image')
-    readonly_fields = ['display_image']
+    fields = ("employee_id", "full_name")
 
 
 
